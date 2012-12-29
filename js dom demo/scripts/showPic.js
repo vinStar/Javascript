9b2fc441b2eba9c8877fd38db4e85a24692e3706
vincent <731315455@qq.com>
2012-12-29 15:50:56 +0800

    pratice  nodeType childNodes nodeValue firstNode lastNode

--- js dom demo/scripts/showPic.js
+++ js dom demo/scripts/showPic.js
@@ -1,7 +1,19 @@
 function showPic(whichpic){
+			
 	var source=whichpic.getAttribute("href");
 	var placeholder=document.getElementById("placeholder");
 	placeholder.setAttribute("src",source);
 	//test commit
+	 var titletext=whichpic.getAttribute("title");
+	 var description=document.getElementById("description");
+	 //alert(description.childNodes[0].nodeValue);
+	description.firstChild.nodeValue=titletext;
 	
+}
+//window.onload=countBodyChildren;
+function countBodyChildren(){
+	var body_element=document.getElementsByTagName("body")[0];
+//	alert("the count of body childElement is "+body_element.childNodes.length);
+	alert(body_element.nodeType);
+
 }

debdc2c24d94c407c11d3eb3d62b478b9e160e96
vincent <731315455@qq.com>
2013-01-06 13:59:57 +0800

    pratice popUp (window.open(p1,p2,p3))

--- js dom demo/scripts/example.js
+++ js dom demo/scripts/example.js
@@ -1,3 +1,25 @@
 var num=7.561;
 num=Math.round(num);
-alert(num);
+//alert(num);
+
+function popUp(winURL){
+
+	window.open(winURL,"popup","width=320,height=480");
+}
+
+function showHref(){
+	var links=document.getElementsByTagName("a");
+	for(var i=0;i<links.length;i++){
+	if(links[i].className=="popup"){
+	links[i].onclick=function(){
+		popUp(this.getAttribute("href"));
+		return false;
+		}
+	}
+	}
+	
+}
+
+//ÏòcssÑ§Ï°
+window.onload=showHref;
+

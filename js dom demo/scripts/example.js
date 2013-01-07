5109b1d64169eacded881d2bfcff83e2b412de54
vinStar <731315455@qq.com>
2013-01-07 23:50:26 +0800

    chapter 7 page 120 commit  to sleep :]

--- js dom demo/scripts/example.js
+++ js dom demo/scripts/example.js
@@ -37,9 +37,64 @@ function insertParagraph(text){
 }
 
 window.onload=function(){
-  var testdiv=document.getElementById("testdiv");
-  alert(testdiv.innerHTML);
+  //var testdiv=document.getElementById("testdiv");
+  //testdiv.innerHTML="<p>I inserted <em>this</em> content.</p>";
+  
+  var para = document.createElement("p");
+  //var info = "nodeName: ";
+  //info+=para.nodeName;
+  //info+=" nodeType: ";
+  //info+=para.nodeType;
+  //alert(info);
+
+//---
+  //var testdiv = document.getElementById("testdiv");
+  
+  //var txt = document.createTextNode("Hello world");
+  //para.appendChild(txt);
+  //var testdiv = document.getElementById("testdiv");
+  //testdiv.appendChild(para);
+
+//---
+  var txt1= document.createTextNode("This is ");
+  para.appendChild(txt1);
+  var emphasis =document.createElement("em");
+  var txt2 = document.createTextNode("my");
+  emphasis.appendChild(txt2);
+  para.appendChild(emphasis);
+  var txt3 = document.createTextNode(" content.");
+  para.appendChild(txt3);
+  var testdiv= document.getElementById("testdiv");
+  testdiv.appendChild(para);
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

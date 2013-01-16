02940457c428866c25d5c9a7de2862587d416c9f
vinStar <731315455@qq.com>
2013-01-17 00:23:22 +0800

    move overflow

---
+++ js dom demo/scripts/moveElement.js
@@ -0,0 +1,34 @@
+function moveElement(elementID,final_x,final_y,interval){
+  
+  if(!document.getElementById(elementID)) return false;
+  var elem = document.getElementById(elementID);
+  //≥ı ºŒª÷√
+  var xpos = parseInt(elem.style.left);
+  var ypos = parseInt(elem.style.top);
+  if(elem.movement){
+  clearTimeout(elem.movement);
+  }
+  if(xpos == final_x && ypos == final_y){
+    return true;
+  }
+
+  if(xpos < final_x ){
+    xpos++;
+  }
+  if(xpos > final_x ){
+    xpos--;
+  }
+  if(ypos < final_y){
+    ypos++;
+  }
+  if(ypos > final_y){
+    ypos--;
+  }
+
+  elem.style.left = xpos + "px";
+  elem.style.top = ypos + "px";
+
+  var repeat  = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+  elem.movement = setTimeout(repeat,interval);
+  
+}
---
+++ js dom demo/scripts/prepareSlideshow.js
@@ -0,0 +1,39 @@
+
+function $(elementId){
+return document.getElementById(elementId);
+}
+
+
+function prepareSlideshow(){
+if(!document.getElementById) return false;
+
+//Make suer the elements exist
+if(!$("linklist")) return false;
+if(!$("preview")) return false;
+
+var preview = $("preview");
+preview.style.position="absolute";
+preview.style.left="0px";
+preview.style.top ="0px";
+
+var list=$("linklist");
+var links = list.getElementsByTagName("a");
+
+links[0].onmouseover=function(){
+  moveElement("preview",-100,0,10);
+}
+
+
+links[1].onmouseover=function(){
+  moveElement("preview",-200,0,10);
+}
+
+
+links[2].onmouseover=function(){
+  moveElement("preview",-300,0,10);
+}
+
+}
+
+addLoadEvent(prepareSlideshow);
+
